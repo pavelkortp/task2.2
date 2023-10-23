93c14ed590c0102cf937981af2c03e10881fa376
Pavlo <pavelkortp@gmail.com>
2023-10-24 01:49:56 +0300

    maybe ready first task

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('Hello');
+async function getIP() {
+    return (await fetch('https://api.ipify.org/?format=json')).json();
+}
+async function main() {
+    const s =  await getIP();
+    console.log(s);
+    
+}
+main();
+
+
+
