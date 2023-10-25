283616f2e57a2abe38a4d9fa661e6249b2a3ab78
Pavlo <pavelkortp@gmail.com>
2023-10-25 17:08:42 +0300

    task 4 ready, but recursive not great approach

---
+++ dist/female.js
@@ -0,0 +1,42 @@
+"use strict";
+const LINK = 'https://random-data-api.com/api/users/random_user';
+/**
+ * Gets a female user for the minimum number of requests
+ * (Using async/await).
+ */
+async function getFemale1() {
+    let counter = 0;
+    let gender;
+    do {
+        gender = (await ((await fetch(LINK)).json())).gender;
+        counter++;
+    } while (gender !== 'Female');
+    console.log(`To find female it takes ${counter} times`);
+}
+/**
+ * Gets user from server.
+ * @returns new user.
+ */
+function getUser() {
+    return fetch(LINK)
+        .then((resp) => resp.json());
+}
+/**
+ * Gets a female user for the minimum number of requests
+ * (Using then()).
+ */
+function getFemale2(counter = 1) {
+    getUser()
+        .then((user) => {
+        counter++;
+        if (user.gender === 'Female') {
+            console.log(`To find female it takes ${counter} times (Recursively)`);
+        }
+        else {
+            getFemale2(counter);
+        }
+    });
+}
+
+getFemale1();
+getFemale2();
---
+++ female.ts
@@ -0,0 +1,45 @@
+const LINK = 'https://random-data-api.com/api/users/random_user';
+
+/**
+ * Gets a female user for the minimum number of requests
+ * (Using async/await).
+ */
+async function getFemale1() {
+    let counter = 0;
+    let gender;
+    do {
+        gender = (await ((await fetch(LINK)).json())).gender;
+        counter++;
+    } while (gender !== 'Female');
+    console.log(`To find female it takes ${counter} times`);
+}
+
+/**
+ * Gets user from server.
+ * @returns new user.
+ */
+function getUser(): Promise<any> {
+    return fetch(LINK)
+        .then((resp: Response) => resp.json())
+}
+
+/**
+ * Gets a female user for the minimum number of requests
+ * (Using then()).
+ */
+function getFemale2(counter: number = 1) {
+    getUser()
+        .then((user) => {
+            counter++;
+            if (user.gender === 'Female') {
+                console.log(`To find female it takes ${counter} times (Recursively)`);
+            } else {
+                getFemale2(counter);
+            }
+        });
+
+}
+
+getFemale1();
+getFemale2();
+
